92215e2c339361f3085c0ceccfd14b1731e4bd32
William Palafox <wpalafox@gmail.com>
2023-07-17 12:05:35 -0400

    25 min sesh

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">Hello Will
+      <Button colorScheme='blue'>BUTTON</Button>
+    </div>
+  );
+}
+
+export default App;
