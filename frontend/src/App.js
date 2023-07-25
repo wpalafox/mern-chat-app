0b56af257d861060ce394e577cb18e523065c2f9
William Palafox <wpalafox@gmail.com>
2023-07-25 07:46:38 -0400

    react routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import { Button } from '@chakra-ui/react';
+import { Route } from "react-router-dom";
+import  HomePage from "./Pages/HomePage.js";
+import ChatPage from "./Pages/ChatPage.js";
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">Hello Will
-      <Button colorScheme='blue'>BUTTON</Button>
+
+       <Route path="/" component={HomePage} />
+       <Route path='/chats' component={ChatPage} /> 
+    
     </div>
   );
 }
